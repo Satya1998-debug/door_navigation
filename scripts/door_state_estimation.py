cda261637dd28c2e58b181ac0b0db4b1878887f6
Satyabrata Pradhan <spradhan143as@gmail.com>
2026-01-06 11:19:18 +0100

    added ollama api with b64

--- scripts/door_state_estimation.py
+++ scripts/door_state_estimation.py
@@ -1,23 +1,51 @@
 import base64
 from os import path
-import requests
+from ollama import chat
+import cv2
 
 ollama_base_url = "http://localhost:11434"
 
-def estimate_door_state_ollama_api(rgb_img, api_url):
-    from ollama import chat
+def estimate_door_state_ollama_api(image_path: str) -> str:
+    try:
+        ok, buf = cv2.imencode('.jpg', cv2.imread(image_path))
+        if not ok:
+            raise RuntimeError(f"Failed to encode image at {image_path}.")
+        rgb_img_bytes = buf.tobytes() 
+        img_b64 = base64.b64encode(rgb_img_bytes).decode('utf-8')
 
-    response = chat(
-        model='gemma3',
-        messages=[
-            {
-            'role': 'user',
-            'content': 'What is in this image? Be concise.',
-            'images': [path],
-            }
-        ],
-    )
+        prompt = "Look at this image. Classify the door state as 'open', 'partly-open', or 'closed'. Also tell if any human is present. " \
+        "Classify door_type (single/double), door_material (normal/glass)" \
+        "Response structure: {'door_state': <state>, 'human_present': <yes/no>, 'door_type': <type>, 'door_material': <material>}. Only respond with the JSON structure."
+
+        response = chat(
+            model='qwen3-vl:4b',
+            messages=[
+                {
+                    'role': 'user',
+                    'content': prompt,
+                    'images': [img_b64],
+                }
+            ],
+            format="json"
+        )
+
+        
+        # print(f"Ollama API response: {response}")
+
+        res = response.message.content.strip().lower()
+        if res:
+            door_state = res
+            print(f"Estimated door state: {door_state}")
+            return door_state
+        else:
+            print("No valid response received from Ollama API.")
+            return None
+    
+    except Exception as e:
+        print(f"Error during estimate_door_state_ollama_api: {e}")
+        return None
 
 
 if __name__ == "__main__":
-    pass
+    path = "/home/RUS_CIP/st184744/codebase/door_navigation/scripts/data_new/latest_image_color_lab_35.jpg"
+    estimate_door_state_ollama_api(path)
